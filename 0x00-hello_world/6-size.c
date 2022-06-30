5d7f9c1ea940fdcfd0a7c013203789fb212a7e69
Reubenspain <sezra86@gmail.com>
2022-06-30 10:13:44 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,21 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-		printf("Size of a char: %i byte(s)\n", sizeof(char));
-			printf("Size of an int: %i byte(s)\n", sizeof(int));
-				printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %i byte(s)\n", sizeof(float));
-							return (0);
+      char charType;
+      int intType;
+      long int lontIntType;
+      long long int longlongType;
+      float flotaType;
+
+      printf("Size of a char: %i byte(s)\n", sizeof(char))
+      printf("Size of an int: %i byte(s)\n", sizeof(int));
+      printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+      printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+      printf("Size of a float: %i byte(s)\n", sizeof(float));
+      return (0);
 }
