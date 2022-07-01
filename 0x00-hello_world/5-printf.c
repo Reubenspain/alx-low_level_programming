1e5f4ca7157bcaff267d8db70a52a3904114733c
Reubenspain <sezra86@gmail.com>
2022-06-30 18:19:53 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
- /**
-  * main - main block
-  * Return: 0
-  */
-
- int main(void)
- {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
- }
+int main()
+{
+	    printf("with proper grammar, but the outcome is a piece of art,\n");
+	        return 0;
+}
