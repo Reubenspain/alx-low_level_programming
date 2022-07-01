098aeedfbabfea22d9f1510eaa6894b2860d33b6
Reubenspain <sezra86@gmail.com>
2022-07-01 06:28:31 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void
+
+int main()
 {
 	    printf("with proper grammar, but the outcome is a piece of art,\n");
 	        return 0;
 }
+
 
