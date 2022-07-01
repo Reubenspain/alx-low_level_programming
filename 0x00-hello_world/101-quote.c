6f2d20ef611b68a828f3c2dfabc0915c07cb3761
Reubenspain <sezra86@gmail.com>
2022-07-01 10:27:20 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- *  * main - Entry point
- *   * Return:1
- *    */
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
+	char var[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, var, sizeof(var) - 1);
 	return (1);
 }
