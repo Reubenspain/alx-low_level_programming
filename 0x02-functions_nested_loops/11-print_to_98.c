db6489c9e4fd1f93c929f2d9f6a4a88318ce9307
Reubenspain <sezra86@gmail.com>
2022-07-05 04:26:46 -0700

    commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_to_98 - writes the character c to stdout
+ * @n: starting number
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+{
+	while (n <= 98)
+{
+	if (n == 98)
+{
+	printf("%d\n", n);
+	break;
+}
+	printf("%d, ", n);
+	n++;
+}
+}
+	else
+{
+	while (n >= 98)
+{
+	if (n == 98)
+{
+	printf("%d\n", n);
+	break;
+}
+	printf("%d, ", n);
+	n--;
+}
+}
+}
