fae868e775096f611ad55f9d69e7673a68a9b853
Reubenspain <sezra86@gmail.com>
2022-07-05 06:34:45 -0700

    commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,2 @@
 int _putchar(char c);
-
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	    print_alphabet();
-	        return (0);
-}
+int print_alphabet(void);
