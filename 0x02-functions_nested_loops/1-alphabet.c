92cb5be14a6e49de16b8cf66a1d22e45b0b2a22c
Reubenspain <sezra86@gmail.com>
2022-07-05 03:26:24 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code for Alx School students.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+{
+	_putchar (alphabet);
+	alphabet++;
+
+}
+	_putchar ('\n');
+}
