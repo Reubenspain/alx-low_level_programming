fc9a015eec92159d8830ed037008d6dbae09c255
Reubenspain <sezra86@gmail.com>
2022-07-07 11:22:47 -0700

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,13 +12,14 @@ void print_diagonal(int n)
 	if (n <= 0)
 {
 	_putchar('\n');
-}	else
+}	
+	else
 {
 	for (i = 0; i < n; i++)
 {
 	for (j = 0; j < i; j++)
 {
-	_putcha(' ');
+	_putchar(' ');
 }
 	_putchar('\\');
 	_putchar('\n');
